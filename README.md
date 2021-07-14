677b962a42da8845d92208a6e0d697c0e0cd7148
Vadim Zhilinkov <dreadct@gmail.com>
2021-07-15 00:31:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Description
+
+A SwiftUI based app for sending APNs push notifications.
---
+++ Slingshot/SlingshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlingshotApp.swift
+//  Slingshot
+//
+//  Created by Vadim Zhilinkov on 15.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlingshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
